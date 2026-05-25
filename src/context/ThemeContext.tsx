d02f9d6fc2b7fde217f1abb5db48a0e4b32b7a02
Karl Keynes Abid <karlkeynes.abid@jti.com.ph>
2026-05-26 07:59:28 +0800

    Finish theme context file for styling in components

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,2 +1,34 @@
-import React, { createContext, useState } from 'react';
-import { gaiaTheme, GaiaTheme } from '../theme';
+import React, { createContext, useContext, useState } from 'react';
+import { gaiaTheme, type GaiaTheme } from '../theme';
+
+// Note about the importation line above^
+// TS gets confused when a TS import of a "type" is joined together in one line with the JS one--because I have "verbatimModuleSyntax" to true.
+const ThemeContext = createContext<GaiaTheme | undefined>(undefined);
+
+export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [currentTheme] = useState<GaiaTheme>(gaiaTheme);
+
+    return (
+        <ThemeContext.Provider value={currentTheme}>
+            <div
+                style={{
+                    backgroundColor: currentTheme.colors.background,
+                    minHeight: '100vh',
+                    color: currentTheme.colors.textMain,
+                    transition: 'background-color 0.2s ease'
+                }}
+            >
+                {children}
+            </div>
+        </ThemeContext.Provider>
+    )
+}
+
+export const useGaiaTheme = () => {
+    const context = useContext(ThemeContext);
+
+    if (!context) {
+        throw new Error('useGaiaTheme must be used within a ThemeProvider');
+    }
+    return context;
+}
