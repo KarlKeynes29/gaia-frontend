1795749e6bb37cbde187ce351729778e58f9e4ac
Karl Keynes Abid <karlkeynes.abid@jti.com.ph>
2026-05-26 17:18:50 +0800

    Push changes relate to theme mostly!

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import { login } from '../context/AuthContext';
+import { useGaiaTheme } from '../context/ThemeContext';
 
 export const Login: React.FC = () => {
 
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,24 @@
 export const gaiaTheme = {
     colors: {
-        primary: '#00953e',
-        background: '#02d158',
-        surface: '#fa9f2f',
-        textMain: '#222824',
-        textLight: '#6B756E',
-        border: '#d6790f'
+        // for key actions
+        primary: '#007A33',
+
+        // price highlights and other accents
+        accent: '#FA9F2F',
+
+        // main background
+        background: '#121412',
+
+        // color for product card surface/background
+        surface: '#1A1D1A',
+
+        // titles
+        textMain: '#FFFFFF',
+
+        // descriptions
+        textLight: '#A0AAA4',
+        // edges
+        border: '#2D332F',
     }
 }
-
 export type GaiaTheme = typeof gaiaTheme;
