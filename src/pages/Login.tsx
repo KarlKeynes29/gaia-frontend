1740c524de6c72a756116f4f1c50d0474e3f6af3
Karl Keynes Abid <karlkeynes.abid@jti.com.ph>
2026-05-28 17:01:01 +0800

    Add css files for base theme of the app.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,10 +3,12 @@ import { login } from '../context/AuthContext';
 import { useGaiaTheme } from '../context/ThemeContext';
 
 export const Login: React.FC = () => {
+    const [userDetails, setUserDetails] = useState(null);
+    const baseURL;
 
     return (
         <>
-            <div>
+            <div className='login-container'>
 
             </div>
         </>
